606ab9c98d268e35f5fc72e8586e3ed31a6f427d
KiWlidee <mihailgasnikov35@gmail.com>
2026-01-15 00:30:19 +0300

    До обязательной покупки

--- main.py
+++ main.py
@@ -4,15 +4,6 @@ import requests
 
 from bs4 import BeautifulSoup
 
-
-def open_page(name: str):
-    """Open page with rent-name"""
-    try:
-        open(f"https://gorent.shop/product/{name}-arenda-akkaunta-steam/")
-        return {"status": "OK"}
-    
-    except Exception as e:
-        return f"Error {e}"
     
 
 def get_soup(name: str) -> BeautifulSoup:
@@ -28,7 +19,8 @@ def get_soup(name: str) -> BeautifulSoup:
             res = requests.get(f"https://gorent.shop/product/{name}-arenda-steam/")
             link_change = True
             if res.status_code != 200:
-                return f"Cannot connect to link"
+                link = f"https://gorent.shop/product/{name}-arenda-steam/"
+                return "Cannot connect to link", link
         soup = BeautifulSoup(res.text, "lxml")
         if link_change:
             return soup, True
@@ -38,7 +30,7 @@ def get_soup(name: str) -> BeautifulSoup:
         return f"Error {e}"
     
 
-def find_product_info(name: str, soup: BeautifulSoup):
+def find_product_info(name: str, soup: BeautifulSoup) -> str:
     button_text = soup[0].find("div", class_="summary entry-summary")
     if "Будет доступен:" in button_text.text:
         return "❌ Аккаунт занят"
@@ -46,18 +38,20 @@ def find_product_info(name: str, soup: BeautifulSoup):
     if soup[1]:
         print("🔎 Opened changed link")
         open(f"https://gorent.shop/product/{name}-arenda-steam/")
-        return "✅ Аккаунт доступен"
     else:
         print("🌐 Opened link")
         open(f"https://gorent.shop/product/{name}-arenda-akkaunta-steam/")
-        return "✅ Аккаунт доступен"
+    return "✅ Аккаунт доступен"
 
 
 
 if __name__  == "__main__":
-    name = input("Введите полное название игры: ")
+    name = input("Введите полное название игры: ").strip()
     name = name.replace(" ", "-")
     name = name.replace(":", "").lower()
+
     soup = get_soup(name)
-    if soup != "Cannot connect to link":
+    if soup[0] != "Cannot connect to link":
         print(find_product_info(name, soup))
+    else:
+        print(f"⚠️  Не удалось открыть ссылку: {soup[1]}")
